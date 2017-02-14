18c180b49e048fdcd5620219175ae718344511eb
Jason Mohabir <jmohabir@stuy.edu>
2017-02-13 20:28:20 -0500

    Fixed the clear button

--- drawing.js
+++ drawing.js
@@ -2,5 +2,13 @@ var c = document.getElementById("slate");
 var ctx = c.getContext("2d");
 
 ctx.fillStyle = "#ff0000";
-ctx.fillRect( 50, 50, 100, 200 );
+
+c.addEventListener("click", function(e) {
+	ctx.fillRect( 50, 50, 100, 200 );	
+    });
+                     
+var clear = document.getElementById("clearButton");
+clear.addEventListener("click", function() {
+	ctx.clearRect(0, 0, c.width, c.height);
+    });
 
